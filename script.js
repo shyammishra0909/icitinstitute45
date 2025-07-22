d7d33014ffce5e7ec895730abc94e8e4e280dff3
shyammishra0909 <akm02482@gmail.com>
2025-07-22 20:23:01 +0530

    add files

---
+++ script.js
@@ -0,0 +1,5 @@
+const userName = prompt("Enter the student name");
+
+    if (userName) {
+      document.getElementById("ak").innerText = "Welcome to ICIT INSTITUTE online registration from, " + userName + "!";
+    }
